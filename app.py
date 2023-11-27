b4d17526ef13ddfbd37da7aace46cc3ffd18fad5
keteb <ketebmehdi2@gmail.com>
2023-11-27 11:35:27 +0100

    begin the interface of the app

--- app.py
+++ app.py
@@ -1,4 +1,28 @@
 import streamlit as st 
+import time
 
 
-st.write('This isap')
+
+st.warning('Warning : the given prediction is by no means a reliable investment !')
+
+crypto_selectbox = st.selectbox('Choose your Cryptocurrencie', 
+                             ('BTC', 'ETH', 'BNB'))
+
+
+select_time = st.radio('Choose the Data interval', ['Hourly', 'Daily'])
+if select_time == 'Hourly':
+    slider_hour = st.slider('Choose the time in hour to predict', 1, 10, 1)
+
+else:
+    select_days = st.slider('Choose the time in days to predict', 1, 10, 1)
+
+button_prediction = st.button('Submit freatures')
+
+if button_prediction:
+    with st.spinner('Prediction in progress ....'):
+        time.sleep(5)
+    st.success('Done!')
+
+    
+
+
