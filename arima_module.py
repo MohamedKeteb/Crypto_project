c2cd88d3844180dd142a4703758096cb2feba7e8
gxba <kokouelvismondesir.gaba-kpayedo@ensae.fr>
2023-12-12 10:42:02 +0000

    RAS

--- arima_module.py
+++ arima_module.py
@@ -1,11 +1,11 @@
 # 01_This function allows to fast-check the stationnarity of the series by plotting the price we are interested in and its rolling mean/standard deviation on the same plot and then computing the ADF test.
 
-def stationnarity_fast_check(data,price, period) : 
-    rolling_mean = data['price'].rolling(window=period).mean()
-    rolling_std = data['price'].rolling(window=period).std()
+def stationnarity_fast_check(data, price, period) : 
+    rolling_mean = data[str(price)].rolling(window=period).mean()
+    rolling_std = data[str(price)].rolling(window=period).std()
     
 # Plot comparing time series and its moving statistics
-    original = plt.plot(data['price'], color='blue', label='Close cotation')
+    original = plt.plot(data[str(price)], color='blue', label= str(price) + 'cotation')
     mean = plt.plot(rolling_mean, color='red', label='Rolling mean')
     std = plt.plot(rolling_std, color='black', label='Rolling standard deviation')
     plt.legend(loc='best')
@@ -13,7 +13,7 @@ def stationnarity_fast_check(data,price, period) :
     plt.show(block=False)
         
 # Dickey–Fuller's Test :
-    result = adfuller(data['price'])
+    result = adfuller(data[str(price)])
     print('ADF statistics : {}'.format(result[0]))
     print('p-value : {}'.format(result[1]))
     print('Critical values :')
